faeb8fcf05a7248378156315102a7e6d90afd9cd
JosephBadua <josephbadua1006@gmail.com>
2019-11-09 11:53:49 -0800

    version1.0

--- models/lostAnimals.js
+++ models/lostAnimals.js
@@ -1,16 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
   var lostAnimals = sequelize.define("Lost_Animals", {
     id: { type: DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
-    animal_name: { type: DataTypes.STRING, validate: {allowNulll:false}, is: ["^[a-z]+$",'i']},
-    species: { type: DataTypes.STRING, validate: {allowNulll:false}, is: ["^[a-z]+$",'i']},
-    breed: { type: DataTypes.STRING, validate: {allowNulll:false}, is: ["^[a-z]+$",'i']},
-    picture: { type: DataTypes.STRING, validate: {allowNulll:false}},
-    color: { type: DataTypes.STRING, validate: {allowNulll:false}, is: ["^[a-z]+$",'i']},
-    animal_description: { type: DataTypes.TEXT, validate: {allowNulll:false}, is: ["^[a-z]+$",'i']},
-    lost_date: { type: DataTypes.DATE, validate: {allowNulll:false}},
-    lost_where: { type: DataTypes.TEXT, validate: {allowNulll:false}},
+    animal_name: { type: DataTypes.STRING, validate: {allowNull: true}, is: ["^[a-z]+$",'i']},
+    species: { type: DataTypes.STRING, validate: {allowNull: true}, is: ["^[a-z]+$",'i']},
+    breed: { type: DataTypes.STRING, validate: {allowNull: true}, is: ["^[a-z]+$",'i']},
+    picture: { type: DataTypes.STRING, validate: {allowNull: true}},
+    color: { type: DataTypes.STRING, validate: {allowNull: true}, is: ["^[a-z]+$",'i']},
+    animal_description: { type: DataTypes.TEXT, validate: {allowNull: true}, is: ["^[a-z]+$",'i']},
+    lost_date: { type: DataTypes.DATE, validate: {allowNull: true}},
+    lost_where: { type: DataTypes.TEXT, validate: {allowNull: true}},
     still_lost: { type: DataTypes.BOOLEAN },
-    zip_code: { type:DataTypes.INTEGER },
+    zip_code: { type:DataTypes.INTEGER, validate: {allowNull:false}},
     contact_email: { type: DataTypes.STRING, validate: {isEmail:true}},
   });
   return lostAnimals;
--- models/seeds.sql
+++ models/seeds.sql
@@ -1,2 +1,22 @@
 USE animals_db;
-INSERT INTO Lost_Animals (animal_name, species, breed, picture, color, animal_description, lost_date, lost_where, still_lost, zip_code, contact_email, createdAt, updatedAt) VALUES ("max", "dog", "husky", "image.jpeg", "black and white", "Big eyes and very small", "2019-11-05", "Riverside", TRUE, 90005,"contact@gmail.com", 0, 0);
+INSERT INTO Lost_Animals (animal_name, species, breed, picture, color, animal_description, lost_date, lost_where, still_lost, zip_code, contact_email, createdAt, updatedAt) 
+VALUES ("Max", "dog", "husky", "https://thehappypuppysite.com/wp-content/uploads/2017/12/siberian6.jpg", "black and white", "Big eyes and very small", "2019-11-05", "Riverside", TRUE, 90005,"dodgers2@gmail.com", 0, 0),
+​
+("Spot", "dog", "terrier", "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/01/12201051/cute-puppy-body-image.jpg", "brown", "small and fuzzy", "2019-11-01", "Menifee", TRUE, 92585,"SD123@gmail.com", 0, 0),
+​
+("Raven", "cat", "siamese", "https://www.thesprucepets.com/thmb/yKUS9NTSH9UxwNXlboSR9dmqzRY=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/37348687_179210919505845_8579658165484781568_n-5b69b1b346e0fb00500b0880.jpg", "black and gray", "short hair and playful", "2019-11-02", "Riverside", TRUE, 92585,"catlover@gmail.com", 0, 0),
+​
+("Simba", "dog", "retriever", "https://www.petmd.com/sites/default/files/senior-golden-retriever-with-ball-picture-id488657289.jpg", "golden brown", "Happy and jumps alot!", "2019-11-02", "Riverside", TRUE, 90005,"lionking@gmail.com", 0, 0),
+​
+("Felix", "cat", "unknown", "https://www.thesprucepets.com/thmb/0tFhy7gu82dgMGL_j5SQvCHXZes=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/close-up-of-cat-lying-on-floor-at-home-908763830-1d61bee6961b45ee8a55bdfa5da1ebb3.jpg", "black", "Big eyes, likes to scratch", "2019-11-10", "Riverside", TRUE, 90005,"felixowner@gmail.com", 0, 0),
+​
+("Powder", "cat", "dont know", "https://www.rover.com/blog/wp-content/uploads/2019/03/white-cat-4025572_1920-960x540.jpg", "white", "lazy, likes to sleep", "2019-11-07", "Menifee", TRUE, 92585,"lightning999@gmail.com", 0, 0),
+​
+("Jack", "dog", "pitbull", "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555296004/shape/mentalfloss/istock_86999965_small.jpg", "white and black", "gets into the trash alot", "2019-11-06", "Menifee", TRUE, 92585,"colorodo999@gmail.com", 0, 0),
+​
+("Fuzzy", "dog", "poodle mix", "https://www.loveyourdog.com/wp-content/uploads/2019/04/Toy-Poodle.jpg", "brown", "likes to play fetch", "2019-11-03", "Riverside", TRUE, 90005,"flash155@gmail.com", 0, 0),
+​
+("Trixie", "cat", "not sure", "https://www.thehappycatsite.com/wp-content/uploads/2018/03/havana-brown-cat.jpg", "brown", "likes to play", "2019-11-03", "Riverside", TRUE, 90005,"catz155@gmail.com", 0, 0),
+​
+("Lady", "dog", "poodle", "https://www.rover.com/blog/wp-content/uploads/2018/11/poodle-960x540.jpg", "white", "Scares easily", "2019-11-02", "Riverside", TRUE, 90005,"tree123@gmail.com", 0, 0);
+​
