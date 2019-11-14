d6835227e56d11bd20c085818892cf304b80cf8b
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2019-11-13 20:53:04 -0800

    adding background gif to index, updated footer

--- routes/email.js
+++ routes/email.js
@@ -1,25 +1,25 @@
 module.exports = function(app){
-  const nodemailer = require('nodemailer');
-  let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      auth: {
-          user: 'spotsanimals@gmail.com',
-          pass: 'Animals1!'
-      }
-  });
-  let mailOptions = {
-      from: 'spotsanimals@gmail.com',
-      to: 'Animals1!',
-      subject: 'Test',
-      text: 'Hello World!'
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error.message);
-      }
-      console.log('success');
-  });
-}
+    const nodemailer = require('nodemailer');
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: 'spotsanimals@gmail.com',
+            pass: 'Animals1!'
+        }
+    });
+    let mailOptions = {
+        from: 'spotsanimals@gmail.com',
+        to: 'spotsanimals@gmail.com',
+        subject: 'Test',
+        text: 'Hello World!'
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error.message);
+        }
+        console.log('success');
+    });
+  }
